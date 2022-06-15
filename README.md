f611e25a8e9ee6f426cbde51fadf0835489476ab
Stefan Seeger <seega@gmx.com>
2022-06-16 01:23:55 +0200

    Add TranslateFormatJsCompiler

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # ngx-translate-formatjs-compiler
-Format.js based @ngx/translate compiler
+
+[Format.js](https://formatjs.io/) based [@ngx/translate](https://github.com/ngx-translate/core) compiler.
+
+## Install
+
+`npm i --save intl-messageformat @ngx-translate/core ngx-translate-formatjs-compiler`
+
+## Integration in @ngx/translate/core
+
+```ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateCompiler, TranslateModule } from '@ngx-translate/core';
+import { TranslateFormatJsCompiler } from 'ngx-translate-formatjs-compiler';
+
+import { AppComponent } from './app';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    TranslateModule.forRoot({
+      compiler: {
+        provide: TranslateCompiler,
+        useClass: TranslateFormatJsCompiler,
+      },
+    }),
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+```
+
+## Limitations
+
+Currently, only flat translation objects are supported
+
+- ✅`{"PARENT_CHILD": "Translation"}`
+- ❌`{"PARENT": {"CHILD": "Translation"}}`
+
+## Further links
+
+- [Supported Syntax](https://formatjs.io/docs/core-concepts/icu-syntax)
+- [ICU Online Editor](https://format-message.github.io/icu-message-format-for-translators/editor.html)
---
+++ projects/lib/src/lib/translate-format-js-compiler.spec.ts
@@ -0,0 +1,90 @@
+import { Component, Input } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {
+  TranslateCompiler,
+  TranslateLoader,
+  TranslateModule,
+} from '@ngx-translate/core';
+import { of } from 'rxjs';
+
+import { TranslateFormatJsCompiler } from './translate-format-js-compiler';
+
+@Component({
+  template: `
+    <div id="select">{{ 'TEST_SELECT' | translate: { text: this.text } }}</div>
+    <div id="plural">{{ 'TEST_PLURAL' | translate: { num: this.num } }}</div>
+    <div id="deep">{{ 'DEEP_TEST' | translate: { num: this.num } }}</div>
+  `,
+})
+class TestVVTranslateParserComponent {
+  @Input() num = 0;
+  @Input() text = '';
+}
+
+describe('VVTranslateParser', () => {
+  let fixture: ComponentFixture<TestVVTranslateParserComponent>;
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestVVTranslateParserComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useValue: {
+              getTranslation: () =>
+                of({
+                  TEST_SELECT:
+                    'Ich wohne in einem {text, select, A {Haus} B {Hotel} C {Keller} other {Garten}}.',
+                  TEST_PLURAL:
+                    'Gib mir {num, plural, =0 {Kein} =1 {Ein} =2 {Zwei} other {mehr}} Bier',
+                  DEEP: {
+                    TEST: 'deep test',
+                  },
+                }),
+            },
+          },
+          defaultLanguage: 'de',
+          useDefaultLang: true,
+          compiler: {
+            provide: TranslateCompiler,
+            useClass: TranslateFormatJsCompiler,
+          },
+        }),
+      ],
+    }).compileComponents();
+  }));
+
+  it.each`
+    text   | translation
+    ${'A'} | ${'Ich wohne in einem Haus.'}
+    ${'B'} | ${'Ich wohne in einem Hotel.'}
+    ${'C'} | ${'Ich wohne in einem Keller.'}
+    ${'D'} | ${'Ich wohne in einem Garten.'}
+    ${'E'} | ${'Ich wohne in einem Garten.'}
+  `('should translate select correctly with $text', ({ text, translation }) => {
+    fixture = TestBed.createComponent(TestVVTranslateParserComponent);
+    fixture.componentInstance.text = text;
+    fixture.detectChanges();
+
+    expect(
+      fixture.debugElement.nativeElement.querySelector('#select').textContent,
+    ).toBe(translation);
+  });
+
+  it.each`
+    num  | translation
+    ${0} | ${'Gib mir Kein Bier'}
+    ${1} | ${'Gib mir Ein Bier'}
+    ${2} | ${'Gib mir Zwei Bier'}
+    ${3} | ${'Gib mir mehr Bier'}
+    ${4} | ${'Gib mir mehr Bier'}
+  `('should translate plural correctly with $num', ({ num, translation }) => {
+    fixture = TestBed.createComponent(TestVVTranslateParserComponent);
+    fixture.componentInstance.num = num;
+    fixture.detectChanges();
+
+    expect(
+      fixture.debugElement.nativeElement.querySelector('#plural').textContent,
+    ).toBe(translation);
+  });
+});
